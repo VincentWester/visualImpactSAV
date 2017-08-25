dc55797f1b906d6a8ceea2a8e4c073943c6d0fa5
VincentWester <thibaultdupuy28111989@gmail.com>
2017-08-25 18:08:37 +0200

    [TD] add PDF Client invoice in admin.py and migrations

--- djangoApp/visualImpactSAV/admin.py
+++ djangoApp/visualImpactSAV/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import SAV_file_status, Reparation_status, SAV_file, Event
+from .models import SAV_file_status, Reparation_status, SAV_file, Event, Pdf_client_invoice_file
 
 admin.site.register(SAV_file_status)
 admin.site.register(Reparation_status)
 admin.site.register(SAV_file)
 admin.site.register(Event)
+admin.site.register(Pdf_client_invoice_file)
