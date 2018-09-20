da55b275232ad47e31b0d22c459a76217d452547
VincentWester <thibaultdupuy28111989@gmail.com>
2018-09-20 21:27:24 +0200

    add sav_file in admin

--- djangoApp/visualImpactSAV/admin.py
+++ djangoApp/visualImpactSAV/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import SAV_file_status
+from .models import SAV_file, SAV_file_status
 
 admin.site.register(SAV_file_status)
+admin.site.register(SAV_file)
