bdb8f2dad690abedb4a295d039ef936e709b819b
VincentWester <thibaultdupuy28111989@gmail.com>
2018-10-28 16:11:44 +0100

    Put some oinfo bold style, add some translations and delete print

--- djangoApp/visualImpactSAV/templatetags/tags.py
+++ djangoApp/visualImpactSAV/templatetags/tags.py
@@ -12,13 +12,8 @@ def have_libelle_from_code(value, type_list):
     tuple_list = ()
     if type_list == 'status':
         tuple_list = constants.SAV_FILE_STATUS_CHOICES
-        print 'ici'
     elif type_list == 'waranty':
         tuple_list = constants.SAV_FILE_WARANTY_TYPE_CHOICES
-        print 'la'
-    
-    print tuple_list
-    print value
 
     for status in tuple_list:
         if value == status[0]:
